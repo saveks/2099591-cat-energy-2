cd91e242e91f0bdde410494aa1c627f1b484db76
saveks <minyt15@yandex.ru>
2024-10-13 01:57:41 +0300

    Выравнивает index по PP, правки в скрипте меню

--- source/script.js
+++ source/script.js
@@ -4,11 +4,11 @@ const navToggle = document.querySelector('.main-navigation__toggle');
 navMain.classList.remove('main-navigation--nojs');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-navigation--close')) {
-    navMain.classList.remove('main-navigation--close');
-    navMain.classList.add('main-navigation--open');
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
   } else {
-    navMain.classList.add('main-navigation--close');
-    navMain.classList.remove('main-navigation--open');
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
   }
 });
