86b5df9911d5e322ac994e8110f1d7b0392e541c
saveks <119594175+saveks@users.noreply.github.com>
2024-10-22 21:23:23 +0300

    Добавляет декоративные элементы (#19)
    * Добавляет счетчик в features
    
    * Меняет сетку для hero в desktop
    
    * Выравнивает элементы в hero, programms
    
    * Добавляет карту, выравнивает features
    
    * Меняет сетку карточки в каталоге, мелкие доработки
    
    * Добавляет микросетки в карточки каталога, выравнивание mobile
    
    * Меняет микросетки в каталоге
    
    * Выравнивает блоки в каталоге
    
    * Добавляет скрипт, мелкие правки
    
    * Добавляет слайдер, выравнивание блоков
    
    * Добавляет полоски у карточек, выравнивание
    
    * Выравнивает index desktop по PP
    
    * Выравнивает index по PP, правки в скрипте меню
    
    * Выравнивает catalog mobile по PP, header
    
    * Позиционирует кота в hero, добавляет состояния кнопкам
    
    * Выравнивает catalog tablet по PP
    
    * Добавляет полоски у заголовков в каталоге, выравнивает блоки
    
    * Выравнивает catalog desktop по PP
    
    * Выравнивает блоки, меняет расчет расстояния в example
    
    * Добавляет состояния стрелкам, заканчивает выравнивание
    
    * Добавляет пин на карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-navigation__toggle');
+
+navMain.classList.remove('main-navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
